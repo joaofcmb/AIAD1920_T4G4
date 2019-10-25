af50cb5ffbbdff9cf99a90a74be8cf68efdd58e7
Rui Guedes <ruiguedes12@hotmail.com>
2019-10-25 14:40:38 +0100

    Add deck and card classes with respective functions

---
+++ src/Game/Card.java
@@ -0,0 +1,25 @@
+package Game;
+
+public class Card {
+
+    /**
+     * Card suit
+     */
+    String suit;
+
+    /**
+     * Card rank
+     */
+    String rank;
+
+    /**
+     * Card constructor
+     * @param suit Card suit
+     * @param rank Card rank
+     */
+    Card(String suit, String rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+}
---
+++ src/Game/Deck.java
@@ -0,0 +1,45 @@
+package Game;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+
+    /**
+     * Decks suits
+     */
+    private String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+    /**
+     * Deck ranks
+     */
+    private String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "Jack", "Queen", "King", "Ace"};
+
+    /**
+     * Deck structure
+     */
+    private ArrayList<Card> deck = new ArrayList<>();
+
+
+    /**
+     * Deck default constructor
+     */
+    Deck() {
+        for (String rank : this.ranks) {
+            for (String suit : this.suits) {
+                this.deck.add(new Card(rank, suit));
+            }
+        }
+
+        Collections.shuffle(this.deck);
+    }
+
+    /**
+     * Retrieves the top card and removes it from deck
+     * @return Top card
+     */
+    Card getCard() {
+        return this.deck.remove(0);
+    }
+}
